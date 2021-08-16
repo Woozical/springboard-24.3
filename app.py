d300877b5885ec976b9e7da1cbc46434c2b540ca
Mark <mark@example.dev>
2021-08-16 13:18:00 -0700

    cupcake model, flask app boilerplate

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YOUR_KEY_HERE'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
--- models.py
+++ models.py
@@ -1,1 +1,18 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DEFAULT_IMG_URL = 'https://tinyurl.com/demo-cupcake'
+
+def connect_db(flask_app):
+    db.app = flask_app
+    db.init_app(flask_app)
+
+class Cupcake(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, nullable=False, default=DEFAULT_IMG_URL)
+    
